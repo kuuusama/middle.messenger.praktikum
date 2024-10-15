eb34350cd0dc4a7d079c62f4303fdc8b9745a4bd
Kuusama <kuuusama@gmail.com>
2024-10-15 12:29:00 +0300

    feat: one-page application built

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
 import './main.scss';
+
+declare global {
+    interface Window { app: any; }
+}
+
+enum ChatState {
+    LOGIN = 0,
+    REGISTER = 1,
+    CHAT = 2,
+    PROFILE = 3,
+}
+
+window.app = {
+    hideAllStates: () => {
+        const states = document.getElementsByClassName('state');
+        Array.from(states).forEach((state) => {
+            state.classList.remove('visible');
+        });        
+    },
+
+    changeState: (state: ChatState) => {
+        console.log(`Target state: ${state}`);
+        window.app.hideAllStates();
+        const states = ['stateLogin', 'stateRegister', 'stateChat', 'stateProfile'];
+        const div = document.getElementById(states[state]);
+        console.log(`target div: `);
+        console.log(div);
+        div?.classList.add('visible');        
+    }
+}
+
+window.app.changeState(ChatState.LOGIN);
