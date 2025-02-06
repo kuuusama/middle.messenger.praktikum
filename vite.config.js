834a64e1cf0ea9899bc7b9b3f74d523530d83922
Kuusama <kuuusama@gmail.com>
2025-02-06 22:15:53 +0300

    assets copy method implemented

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+    plugins: [
+        viteStaticCopy({
+          targets: [
+            {
+              src: 'assets',
+              dest: ''
+            }
+          ]
+        })
+    ],
     esbuild: {
         keepNames: true
     },
