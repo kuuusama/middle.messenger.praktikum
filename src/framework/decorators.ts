0fdc85efdea764a46f0a20aa201cb958dd3d7f4d
Kuusama <kuuusama@gmail.com>
2025-02-06 22:37:22 +0300

    minor fix 2

--- src/framework/decorators.ts
+++ src/framework/decorators.ts
@@ -25,20 +25,4 @@ export function Component(options: IComponentConfig) {
     };
 }
 
-// export function Singletone<T extends new (...args: any[]) => any>(ctr: T): T {
-
-//     let instance: T;
-
-//     return class {
-//         constructor(...args: any[]) {
-
-//             if (instance) {
-//                 return instance;
-//             }
-
-//             instance = new ctr(...args);
-//             return instance;
-//         }
-//     } as T
-// }
 
